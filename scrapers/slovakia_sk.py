899f53fcbf9c1b92ecc3d98b1507a4070d5bcf4b
Aziroshin <aziroshin@gmx.net>
2022-02-28 06:14:08 +0100

    Some jerry-rig code for saving local website copies to poke around in.

--- scrapers/slovakia_sk.py
+++ scrapers/slovakia_sk.py
@@ -23,20 +23,61 @@ SLOVAKIA_POINTS_URL = "https://www.minv.sk/?hranicne-priechody-1"
 SLOVAKIA_KML = "https://www.google.com/maps/d/kml?forcekml=1&mid=1umLgEK-j5BHcJAvRBZMFtWztNzhWwgoP"
 
 
+###########################################################################
+# Some functions for quick & dirty prototyping.
+# These functions are not designed to be used for production code, so,
+# removing them and anything that depends on them shouldn't be a problem.
+
+def _save_points_website_working_copy(content: BeautifulSoup) -> None:
+    with open("./.local.slovak_raw_site", "w") as f:
+        f.write(str(content))
+
+
+def _get_points_website_from_working_copy() -> BeautifulSoup:
+    with open("./.local.slovak_raw_site", "r") as f:
+        return BeautifulSoup(f.read(), "lxml")
+
+
+def _print_to_file(message: str) -> None:
+    with open("./.local.slovakia.out", "a") as f:
+        f.write(str(message) + "\n")
+
+###########################################################################
+
+
 class SlovakiaScraper(BaseScraper):
 
     def scrape(self, event = ""):
         print("Scraping Slovakia (SK)")
 
+        ###################################################################
+        # This is not needed to operate the scraper and can be removed
+        # at any time (just make sure you uncomment the
+        # `content = get_website_content(...` line further below).
+
+        """"Rag-tag way to cache a local copy of the website for development"""
+
+        # Uncomment this to save a fresh copy to poke around in:
+        # _save_points_website_working_copy(get_website_content(SLOVAKIA_POINTS_URL))
+
+        # Uncomment this to get the website content from the working
+        # copy file, given that you've run the
+        # `_save_website_working_copy(...` line at least once:
+        content = _get_points_website_from_working_copy()
+
+        ###################################################################
+
         """Start with general border info"""
 
-        content = get_website_content(SLOVAKIA_POINTS_URL)
+        # Uncomment this for production or live testing.
+        # Comment it if you're using the above "rag-tag caching" method.
+        # content = get_website_content(SLOVAKIA_POINTS_URL)
+
         general = self._get_general(content)
 
         """Get border crossing points"""
 
         reception_arr = self._get_reception_points()
-
         # TODO: Replace `""` with the proper URL, once it's clear which one that is.
         write_to_dynamo("slovakia-sk", event, general, reception_arr, "")
 
